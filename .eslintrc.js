5b5add3bcf8ae7306634f27669a3c52f89976257
Your Name <you@example.com>
2024-04-02 01:29:20 +0200

    Add TypeScript configuration for @typescript-nameof/types

--- .eslintrc.js
+++ .eslintrc.js
@@ -114,6 +114,10 @@ module.exports = {
 		  "rules": {
 			"max-lines-per-function": "off"
 		  }
-		}
+		},
+		{
+			extends: ['plugin:@typescript-eslint/disable-type-checked'],
+			files: ["./node_modules/@typescript-nameof/types/index.d.cts"],
+		},
 	  ]
 };
